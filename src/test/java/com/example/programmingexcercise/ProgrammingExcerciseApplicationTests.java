1a99b4413676d6f86d1e46ec07992c6f0dcc9df9
JSmooVE3990 <Jurrand.Summerville.08@gmail.com>
2024-02-19 11:49:32 -0500

    Initial commit

---
+++ src/test/java/com/example/programmingexcercise/ProgrammingExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.programmingexcercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingExcerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
